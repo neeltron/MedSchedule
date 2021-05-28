4f1db2da2480d2e5e08a18c0b09cebc3ffb3328b
unknown <500075823@stu.upes.ac.in>
2021-05-28 23:58:24 +0530

    Added second slot

--- ReMedy_NodeMCU/ReMedy_NodeMCU.ino
+++ ReMedy_NodeMCU/ReMedy_NodeMCU.ino
@@ -6,11 +6,17 @@ Servo slot2;
 void setup() {
   slot1.attach(2);
   slot1.write(0);
+  slot2.attach(4);
+  slot2.write(0);
 }
 
 void loop() {
   slot1.write(90);
   delay(1000);
   slot1.write(0);
+  delay(1000);
+  slot2.write(90);
+  delay(1000);
+  slot2.write(0);
   delay(1000);
 }
