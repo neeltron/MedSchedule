6f902e8cc90a351fcdb6e9e35b83b49da37d4af9
unknown <500075823@stu.upes.ac.in>
2021-05-28 23:44:06 +0530

    Connecting and Testing Slot 1

--- ReMedy_NodeMCU/ReMedy_NodeMCU.ino
+++ ReMedy_NodeMCU/ReMedy_NodeMCU.ino
@@ -1,7 +1,16 @@
-void setup() {
+#include<Servo.h>
 
+Servo slot1;
+Servo slot2;
+
+void setup() {
+  slot1.attach(2);
+  slot1.write(0);
 }
 
 void loop() {
-
+  slot1.write(90);
+  delay(1000);
+  slot1.write(0);
+  delay(1000);
 }
