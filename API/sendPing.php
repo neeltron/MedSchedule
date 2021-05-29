d77c086fc5f76a2d910fa9eff539971821e27047
Neel Adwani <nprosofficial@gmail.com>
2021-05-29 13:18:29 +0530

    Added Time Limit

--- API/sendPing.php
+++ API/sendPing.php
@@ -6,14 +6,19 @@ $link = mysqli_connect("**********", "**********", "**********", "neeltron");
 $sql1 = "SELECT * FROM med WHERE slot = 1 ORDER BY id desc LIMIT 1;";
 $sql2 = "SELECT * FROM med where slot = 2 ORDER BY id desc LIMIT 1;";
 $res1 = mysqli_query($link, $sql1);
+$val = 0;
+
 if (mysqli_num_rows($res1) > 0) {
   while($row = mysqli_fetch_assoc($res1)) {
   	$time = $row['time'];
   	$slot = $row['slot'];
   	$time_updated = strtotime($time);
 	$newTime = date('Hi', $time_updated);
-  	if((int) date('Hi') > (int) $newTime) {
-    	echo "1";
+  	if((int) date('Hi') > (int) $newTime + 2) {
+    	$val = 0;
+    }
+  	else if((int) date('Hi') > (int) $newTime) {
+    	$val = 1;  
     }
   }
 }
@@ -24,10 +29,14 @@ if (mysqli_num_rows($res2) > 0) {
   	$slot = $row['slot'];
   	$time_updated = strtotime($time);
 	$newTime = date('Hi', $time_updated);
-  	if((int) date('Hi') > (int) $newTime) {
-    	echo "2";
+  	if((int) date('Hi') > (int) $newTime + 2) {
+    	$val = 0;
+    }
+  	else if((int) date('Hi') > (int) $newTime) {
+    	$val = 2;
     }
   }
 }
+echo $val;
 
 ?>
